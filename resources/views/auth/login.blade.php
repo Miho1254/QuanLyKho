3849fd3a366402fc705929351a709d44a98f8288
letri2604 <let56800@gmail.com>
2024-06-10 14:50:13 +0700

    hehehe

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    @vite('resources/css/app.css')
+    @vite(['resources/css/app.css','resources/js/app.js'])
 </head>
 <body class="bg-gray-50 dark:bg-gray-900">
     <section class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
