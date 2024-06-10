948c4fb21c36648b604a607e1d2baff90e0c0ae7
HitoruAkame <pannl7746@gmail.com>
2024-06-10 11:25:51 +0700

    .

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,21 +6,37 @@
     <title>Login</title>
     @vite('resources/css/app.css')
 </head>
-<body class="bg-gray-100 flex items-center justify-center h-screen">
-    <div class="bg-white p-8 rounded shadow-md w-96">
-        <h2 class="text-2xl font-semibold mb-6">Login</h2>
-        <form method="POST" action="{{ url('/login') }}">
-            @csrf
-            <div class="mb-4">
-                <label for="email" class="block text-gray-600 font-semibold mb-2">Email:</label>
-                <input type="email" id="email" name="email" required class="w-full border-gray-300 rounded-md p-2 focus:outline-none focus:border-blue-400">
+<body class="bg-gray-50 dark:bg-gray-900">
+    <section class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+            <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+                <h1 class="text-center text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                    Đăng Nhập
+                </h1>
+                <form class="space-y-4 md:space-y-6" method="POST" action="{{ url('/login') }}">
+            	@csrf
+                    <div>
+                        <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Tên đăng nhập</label>
+                        <input type="name" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
+                    </div>
+                    <div>
+                        <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Mật Khẩu</label>
+                        <input type="password" name="password" id="password"  class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
+                    </div>
+                    <div class="flex items-center justify-between">
+                        <div class="flex items-start">
+                            <div class="flex items-center h-5">
+                              <input id="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required="">
+                            </div>
+                            <div class="ml-3 text-sm">
+                              <label for="remember" class="text-gray-500 dark:text-gray-300">Nhớ mật khẩu</label>
+                            </div>
+                        </div>
+                    </div>
+                    <button type="submit" class="w-full bg-blue-500 hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-500 dark:hover:bg-blue-600 dark:focus:ring-blue-700">Sign in</button>
+                </form>
             </div>
-            <div class="mb-6">
-                <label for="password" class="block text-gray-600 font-semibold mb-2">Password:</label>
-                <input type="password" id="password" name="password" required class="w-full border-gray-300 rounded-md p-2 focus:outline-none focus:border-blue-400">
-            </div>
-            <button type="submit" class="w-full bg-blue-500 text-white font-semibold py-2 rounded-md hover:bg-blue-600">Login</button>
-        </form>
-    </div>
+        </div>
+    </section>
 </body>
 </html>
