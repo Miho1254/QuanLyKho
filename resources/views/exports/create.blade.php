3e7a231b054b3fd12b46763d3a26ce38a1cb0c04
letri2604 <let56800@gmail.com>
2024-06-20 14:49:29 +0700

    Fix: xong file index va create cua exports

--- resources/views/exports/create.blade.php
+++ resources/views/exports/create.blade.php
@@ -7,7 +7,12 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Xuất kho</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-    
+    <style>
+        .product-table-container {
+            height: 237px; /* Đặt chiều cao cố định cho phần Danh Sách Sản Phẩm */
+            overflow-y: auto; /* Cho phép cuộn dọc khi vượt quá chiều cao */
+        }
+    </style>
 </head>
 
 @extends('layouts.app')
@@ -16,11 +21,11 @@
         <div class="" style="height: 60%; width: 100%;">
             <div class="" style="height: 100%; width: 100%; display: flex">
                 @include('layouts.sidebar')
-                <div class=" content w-full dark:bg-gray-900">
-                    <div class="relative shadow-md sm:rounded-lg">
+                <div class="content w-full dark:bg-gray-900" style="height: 666px;">
+                    <div class="relative shadow-md sm:rounded-lg" style="height: 100%;">
 
-                        <div class="max-w mx-autop-8 rounded shadow">
-                            <h1 class="text-2xl font-bold mb-6 text-white">Tạo Phiếu Nhập Kho</h1>
+                        <div class="max-w mx-autop-8 rounded shadow" style="height: 100%;">
+                            <h1 class="text-2xl font-bold mb-6 text-white px-5">Tạo Phiếu Xuất Kho</h1>
                             @if(session('success'))
                                 <div class="bg-green-500 text-white p-4 mb-4">
                                     {{ session('success') }}
@@ -33,19 +38,15 @@
                                     <input type="text" name="receipt_id" id="receipt_id" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
                                 </div>
                                 <div class="mb-4">
-                                    <label for="product_type" class="block text-sm font-medium text-white">Loại Sản Phẩm</label>
-                                    <select name="product_type" id="product_type" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
-                                        <option value="Loại sản phẩm 1">Loại sản phẩm 1</option>
-                                        <option value="Loại sản phẩm 2">Loại sản phẩm 2</option>
-                                        <option value="Loại sản phẩm 3">Loại sản phẩm 3</option>
-                                        <!-- Thêm các loại sản phẩm khác vào đây -->
-                                    </select>
+                                    <label for="product_type" class="block text-sm font-medium text-white">Hành động</label>
+                                    <p id="product_type" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white">Xuất kho</p>
                                 </div>
                                 <div class="mb-4">
                                     <label for="products" class="block text-sm font-medium text-white">Sản Phẩm</label>
                                     <div id="product-list">
                                         <div class="flex mb-2">
                                             <select name="product_name" id="product_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                                                <option value="">Chọn sản phẩm</option>
                                                 <option value="Sản phẩm 1">Sản phẩm 1</option>
                                                 <option value="Sản phẩm 2">Sản phẩm 2</option>
                                                 <option value="Sản phẩm 3">Sản phẩm 3</option>
@@ -55,27 +56,24 @@
                                         </div> 
                                     </div>
                                 </div>
-                                <button type="button" id="add-product" class="ml-2 px-4 py-2 bg-blue-500 text-white rounded shadow">Thêm sản phẩm</button>
-                                <button type="submit" class="px-4 py-2 bg-green-500 text-white rounded shadow">Tạo Phiếu</button>
+                                <button type="button" id="add-product" class=" px-5 py-2 bg-blue-500 text-white rounded shadow">Thêm sản phẩm</button>
+                                <button type="submit" class="px-5 ml-2 py-2 bg-green-500 text-white rounded shadow">Tạo Phiếu</button>
                             </form>
                             <div class="mt-6">
-                                <h2 class="text-xl font-bold mb-4 text-white">Danh Sách Sản Phẩm</h2>
-                                <table class="px-5 w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 border-separate">
-                                    <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                                        <tr>
-                                            <th scope="col" class="px-6 py-3 border-b dark:border-gray-600">Loại Sản Phẩm</th>
-                                            <th scope="col" class="px-6 py-3 border-b dark:border-gray-600">Tên Sản Phẩm</th>
-                                            <th scope="col" class="px-6 py-3 border-b dark:border-gray-600">Số Lượng</th>
-                                            <th scope="col" class="px-6 py-3 border-b dark:border-gray-600">Tổng Tiền</th>
-                                            <th scope="col" class="px-6 py-3 border-b dark:border-gray-600">Thao Tác</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody id="product-table">
-                                        <!-- Các sản phẩm sẽ được thêm vào đây -->
-                                    </tbody>
-                                </table>
-                                <div class=" px-5 text-right mt-4 text-white">
-                                    <strong>Tổng Tiền:</strong> <span id="total-amount">0</span> VND
+                                <h2 class="text-xl font-bold mb-4 text-white px-5">Danh Sách Sản Phẩm</h2>
+                                <div class="product-table-container">
+                                    <table class="px-5 w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 border-separate">
+                                        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                            <tr>
+                                                <th scope="col" class="px-6 py-3 border-b dark:border-gray-600">Tên Sản Phẩm</th>
+                                                <th scope="col" class="px-6 py-3 border-b dark:border-gray-600">Số Lượng</th>
+                                                <th scope="col" class="px-6 py-3 border-b dark:border-gray-600">Thao Tác</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody id="product-table">
+                                            <!-- Các sản phẩm sẽ được thêm vào đây -->
+                                        </tbody>
+                                    </table>
                                 </div>
                             </div>
                         </div>
@@ -84,7 +82,6 @@
                             document.getElementById('add-product').addEventListener('click', function() {
                                 var productName = document.getElementById('product_name').value;
                                 var productQuantity = parseInt(document.getElementById('product_quantity').value);
-                                var productType = document.getElementById('product_type').value;
                     
                                 if (!productName || !productQuantity || productQuantity <= 0) {
                                     alert('Vui lòng nhập đầy đủ thông tin sản phẩm với số lượng hợp lệ.');
@@ -95,10 +92,8 @@
                                 var newRow = document.createElement('tr');
                     
                                 newRow.innerHTML = `
-                                    <td class="border px-4 py-2">${productType}</td>
                                     <td class="border px-4 py-2">${productName}</td>
                                     <td class="border px-4 py-2">${productQuantity}</td>
-                                    <td class="border px-4 py-2"></td>
                                     <td class="border px-4 py-2">
                                         <button type="button" class="bg-blue-500 text-white px-4 py-1 rounded mr-2" onclick="editProduct(this)">Sửa</button>
                                         <button type="button" class="bg-red-500 text-white px-4 py-1 rounded" onclick="deleteProduct(this)">Xóa</button>
@@ -106,56 +101,44 @@
                                 `;
                                 tableBody.appendChild(newRow);
                     
-                                updateTotalAmount();
+                                // Sắp xếp lại bảng
+                                sortTable();
                     
                                 // Clear the inputs after adding the product
                                 document.getElementById('product_name').value = '';
                                 document.getElementById('product_quantity').value = '';
                             });
                     
-                            function updateTotalAmount() {
+                            function sortTable() {
                                 var tableBody = document.getElementById('product-table');
-                                var rows = tableBody.getElementsByTagName('tr');
-                                var totalAmount = 0;
-                    
-                                for (var i = 0; i < rows.length; i++) {
-                                    var cells = rows[i].getElementsByTagName('td');
-                                    var productTotal = parseFloat(cells[3].textContent);
-                                    totalAmount += productTotal;
-                                }
-                    
-                                document.getElementById('total-amount').textContent = totalAmount;
+                                var rows = Array.from(tableBody.rows);
+                                rows.sort(function(a, b) {
+                                    var productNameA = a.cells[0].innerText.toUpperCase();
+                                    var productNameB = b.cells[0].innerText.toUpperCase();
+                                    return (productNameA < productNameB) ? -1 : (productNameA > productNameB) ? 1 : 0;
+                                });
+                                rows.forEach(function(row) {
+                                    tableBody.appendChild(row);
+                                });
                             }
                     
                             function editProduct(button) {
                                 var row = button.closest('tr');
                                 var cells = row.getElementsByTagName('td');
-                                var productType = cells[0].textContent;
-                                var productName = cells[1].textContent;
-                                var productQuantity = cells[2].textContent;
+                                var productName = cells[0].textContent;
+                                var productQuantity = cells[1].textContent;
                     
                                 // Populate form with selected product data
-                                document.getElementById('product_type').value = productType;
                                 document.getElementById('product_name').value = productName;
                                 document.getElementById('product_quantity').value = productQuantity;
                     
                                 // Remove the current row from table
                                 row.remove();
-                                updateTotalAmount();
                             }
                     
                             function deleteProduct(button) {
                                 var row = button.closest('tr');
-                                var cells = row.getElementsByTagName('td');
-                                var productTotal = parseFloat(cells[3].textContent);
-                    
-                                // Remove the current row from table
                                 row.remove();
-                    
-                                // Update total amount
-                                var currentTotalAmount = parseFloat(document.getElementById('total-amount').textContent);
-                                var newTotalAmount = currentTotalAmount - productTotal;
-                                document.getElementById('total-amount').textContent = newTotalAmount;
                             }
                     
                             document.getElementById('warehouse-form').addEventListener('submit', function(event) {
@@ -166,14 +149,8 @@
                     
                                 for (var i = 0; i < rows.length; i++) {
                                     var cells = rows[i].getElementsByTagName('td');
-                                    var productType = cells[0].textContent;
-                                    var productName = cells[1].textContent;
-                                    var productQuantity = cells[2].textContent;
-                    
-                                    var productTypeInput = document.createElement('input');
-                                    productTypeInput.type = 'hidden';
-                                    productTypeInput.name = 'products[' + i + '][type]';
-                                    productTypeInput.value = productType;
+                                    var productName = cells[0].textContent;
+                                    var productQuantity = cells[1].textContent;
                     
                                     var productNameInput = document.createElement('input');
                                     productNameInput.type = 'hidden';
@@ -185,7 +162,6 @@
                                     productQuantityInput.name = 'products[' + i + '][quantity]';
                                     productQuantityInput.value = productQuantity;
                     
-                                    form.appendChild(productTypeInput);
                                     form.appendChild(productNameInput);
                                     form.appendChild(productQuantityInput);
                                 }
